5bf6209bac789c85d21db00ac6070c2fa8883bf2
Andrew Kane <andrew@ankane.org>
2023-06-03 16:18:07 -0700

    Added test for Sequel models

--- test/sequel_test.rb
+++ test/sequel_test.rb
@@ -10,17 +10,27 @@ DB.create_table :sequel_items do
   column :embedding, "vector(3)"
 end
 
+class Item < Sequel::Model(DB[:sequel_items])
+end
+
 class TestSequel < Minitest::Test
   def setup
     items.delete
   end
 
-  def test_works
+  def test_dataset
     items.insert(embedding: Pgvector.encode([1, 1, 1]))
     items.multi_insert([{embedding: "[2,2,2]"}, {embedding: "[1,1,2]"}])
     results = items.order(Sequel.lit("embedding <-> ?", Pgvector.encode([1, 1, 1]))).limit(5).all
-    assert_equal [1, 3, 2], results.map { |r| r[:id] }
     assert_equal ["[1,1,1]", "[1,1,2]", "[2,2,2]"], results.map { |r| r[:embedding] }
+  end
+
+  def test_model
+    Item.create(embedding: Pgvector.encode([1, 1, 1]))
+    Item.create(embedding: Pgvector.encode([2, 2, 2]))
+    Item.create(embedding: Pgvector.encode([1, 1, 2]))
+    results = Item.order(Sequel.lit("embedding <-> ?", Pgvector.encode([1, 1, 1]))).limit(5)
+    assert_equal ["[1,1,1]", "[1,1,2]", "[2,2,2]"], results.map(&:embedding)
   end
 
   def items
