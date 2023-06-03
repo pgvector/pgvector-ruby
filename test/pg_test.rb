c363d24fe7f1284c5fd2db54ad01867f3c146852
Andrew Kane <andrew@ankane.org>
2023-06-03 11:52:58 -0700

    Updated table name [skip ci]

--- test/pg_test.rb
+++ test/pg_test.rb
@@ -2,23 +2,23 @@ require_relative "test_helper"
 
 class PgTest < Minitest::Test
   def setup
-    conn.exec("DELETE FROM items")
+    conn.exec("DELETE FROM pg_items")
   end
 
   def test_text
     embedding = [1.5, 2, 3]
-    conn.exec_params("INSERT INTO items (embedding) VALUES ($1), (NULL)", [embedding])
+    conn.exec_params("INSERT INTO pg_items (embedding) VALUES ($1), (NULL)", [embedding])
 
-    res = conn.exec("SELECT * FROM items ORDER BY id").to_a
+    res = conn.exec("SELECT * FROM pg_items ORDER BY id").to_a
     assert_equal embedding, res[0]["embedding"]
     assert_nil res[1]["embedding"]
   end
 
   def test_binary
     embedding = [1.5, 2, 3]
-    conn.exec_params("INSERT INTO items (embedding) VALUES ($1), (NULL)", [embedding])
+    conn.exec_params("INSERT INTO pg_items (embedding) VALUES ($1), (NULL)", [embedding])
 
-    res = conn.exec_params("SELECT * FROM items ORDER BY id", [], 1).to_a
+    res = conn.exec_params("SELECT * FROM pg_items ORDER BY id", [], 1).to_a
     assert_equal embedding, res[0]["embedding"]
     assert_nil res[1]["embedding"]
   end
@@ -30,8 +30,8 @@ class PgTest < Minitest::Test
       unless conn.exec("SELECT 1 FROM pg_extension WHERE extname = 'vector'").any?
         conn.exec("CREATE EXTENSION IF NOT EXISTS vector")
       end
-      conn.exec("DROP TABLE IF EXISTS items")
-      conn.exec("CREATE TABLE items (id bigserial primary key, embedding vector(3))")
+      conn.exec("DROP TABLE IF EXISTS pg_items")
+      conn.exec("CREATE TABLE pg_items (id bigserial primary key, embedding vector(3))")
 
       registry = PG::BasicTypeRegistry.new.define_default_types
       Pgvector::PG.register_vector(registry)
